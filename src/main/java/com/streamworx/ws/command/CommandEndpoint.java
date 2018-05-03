ae8f0ee036540008279c1168100b633f79188d5c
Mon <monica@streamworxsolutions.com>
2018-05-03 18:31:14 +0700

    update code

--- src/main/java/com/streamworx/ws/command/CommandEndpoint.java
+++ src/main/java/com/streamworx/ws/command/CommandEndpoint.java
@@ -1,10 +1,21 @@
 package com.streamworx.ws.command;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
 import com.stx.commands.GetCommandRequest;
 import com.stx.commands.GetCommandResponse;
 
@@ -15,11 +26,67 @@ public class CommandEndpoint {
 
   @PayloadRoot(namespace = "http://stx.com/commands", localPart = "GetCommandRequest")
   @ResponsePayload
-  public GetCommandResponse processCourseDetailsRequest(@RequestPayload GetCommandRequest request) {
+  public GetCommandResponse processCourseDetailsRequest(@RequestPayload GetCommandRequest request) throws IOException {
     GetCommandResponse response = new GetCommandResponse();   
     response.setStatusCode(0);
     response.setStatusDescription("SUCCESS");
-    response.setResult("http://"+request.getIp()+":"+request.getPort()+" >>> "+request.getCommand());
+    boolean success;
+    
+    JSch jsch = new JSch();
+    try {
+
+        Session session = jsch.getSession(request.getUsername(), request.getIp(), request.getPort());
+        session.setConfig("StrictHostKeyChecking", "no");
+        session.setPassword(request.getPassword());
+        session.connect();
+        System.out.println("session connected");
+
+        Channel channel = session.openChannel("exec");
+        ((ChannelExec)channel).setCommand(request.getCommand());
+        channel.setInputStream(null);
+        ((ChannelExec)channel).setErrStream(System.err);
+         
+        InputStream input = channel.getInputStream();
+        channel.connect();
+         
+        System.out.println("Channel Connected to machine " + request.getIp() + " server with command: " + request.getCommand() ); 
+         
+        try{
+            InputStreamReader inputReader = new InputStreamReader(input);
+            BufferedReader bufferedReader = new BufferedReader(inputReader);
+            String line = null;
+             
+            while((line = bufferedReader.readLine()) != null){
+                System.out.println(line);
+            }
+            bufferedReader.close();
+            inputReader.close();
+        }catch(IOException ex){
+            ex.printStackTrace();
+        }
+        success=true;
+
+    } catch (JSchException e) {
+        System.out.println("failed");
+        success=false;
+        e.printStackTrace();
+
+    }
+
+	response.setResult("http://"+request.getIp()+":"+request.getPort()+" >>> "+request.getCommand());
+    if(success) {
+    	response.setStatusCode(0);
+    	response.setStatusDescription("Success");
+    }
+    else
+    {
+    	response.setStatusCode(1);    	
+    	response.setStatusDescription("Error");
+    	response.setErrorCode(999);
+    	response.setErrorMessage("Super Failed");
+    	
+
+    }
     
     
     return response;
